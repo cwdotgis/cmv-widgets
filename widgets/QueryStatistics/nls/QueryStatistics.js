c1ba5a304518ab1258a5ee362d7048aec6c7e4b5
cwdotgis <cwdotgis@gmail.com>
2016-08-16 16:46:17 -0700

    Add QueryStatistics widget files

---
+++ widgets/QueryStatistics/nls/QueryStatistics.js
@@ -0,0 +1,15 @@
+// http://dojotoolkit.org/reference-guide/1.10/dojo/i18n.html
+define({
+    root: {
+    	labels: {
+			numBlocks: 'Block Groups',
+			minDensity: 'Min Density',
+			maxDensity: 'Max Density',
+			avgDensity: 'Average Density',
+			sdDensity: 'Standard Deviation',
+        }
+//		,
+//        Buttons: {
+//        }
+	}
+});
