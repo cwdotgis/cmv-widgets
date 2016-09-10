d6a125ca5f43a18dab5bd5e6525d56d894d9d57d
cwdotgis <cwdotgis@gmail.com>
2016-09-09 17:54:50 -0700

    Edit ids

--- widgets/statisticsConfig.js
+++ widgets/statisticsConfig.js
@@ -3,51 +3,47 @@ define({
     url: 'http://services3.arcgis.com/LjxW17ryOxd5Lbl9/arcgis/rest/services/CaliforniaGeochemicalAnalysis/FeatureServer/0',
     fields: ['OBJECTID', 'ag_icp40', 'as_icp40', 'be_icp40', 'cd_icp40', 'co_icp40', 'cr_icp40', 'cu_icp40', 'pb_icp40', 'th_icp40', 'zn_icp40'],
 	unitsText: 'ppm',
-
     layers: [
-        {
+    {
         name: 'Silver (Ag) by ICP',
-        id: 'ag',
+        id: 'Ag',
         field: 'ag_icp40'
     }, {
         name: 'Arsenic (As) by ICP',
-        id: 'as',
+        id: 'As',
         field: 'as_icp40'
     }, {
         name: 'Beryllium (Be) by ICP',
-        id: 'be',
+        id: 'Be',
         field: 'be_icp40'
     }, {
         name: 'Cadmium (Cd) by ICP',
-        id: 'cd',
+        id: 'Cd',
         field: 'cd_icp40'
     }, {
         name: 'Cobalt (Co) by ICP',
-        id: 'co',
+        id: 'Co',
         field: 'co_icp40'
     }, {
         name: 'Chromium (Cr) by ICP',
-        id: 'cr',
+        id: 'Cr',
         field: 'cr_icp40'
     }, {
         name: 'Copper (Cu) by ICP',
-        id: 'cu',
+        id: 'Cu',
         field: 'cu_icp40'
     }, {
         name: 'Lead (Pb) by ICP',
-        id: 'pb',
+        id: 'Pb',
         field: 'pb_icp40'
     }, {
         name: 'Thallium (Th) by ICP',
-        id: 'th',
+        id: 'Th',
         field: 'th_icp40'
     }, {
         name: 'Zinc (Zn) by ICP',
-        id: 'zn',
+        id: 'Zn',
         field: 'zn_icp40'   
-	    }
-	]
+    }
+    ]
 });
-
-
-
